6318705c4db47fd8d79e726beff2ceecc799394d
Sofia_B <mmeslimani.s@gmail.com>
2023-05-07 11:04:13 +0200

    wild-series

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,27 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
+{
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+        ]);
+    }
+
+
+    #[Route('/{id}', methods:['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/quest5.html.twig', ['id'=> $id]);
+    }
+}
